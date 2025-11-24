a56084fe538c0b1dba9bebcbf79173f7950878bf
Sergiu Vlad Bonta <bonta.vlad@gmail.com>
2025-11-24 15:21:56 +0200

    Add generated.nim

---
+++ src/generated.nim
@@ -0,0 +1,10 @@
+
+{.warning[UnusedImport]: off.}
+{.hint[XDeclaredButNotUsed]: off.}
+from macros import hint, warning, newLit, getSize
+
+from os import parentDir
+
+when not declared(ownSizeOf):
+  macro ownSizeof(x: typed): untyped =
+    newLit(x.getSize)
